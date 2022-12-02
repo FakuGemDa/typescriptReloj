46a6780b4550fb2cdb3f218b57053189ea37f220
FakuGemDa <fakundi@hotmail.com>
2022-12-02 13:49:36 -0300

    Se agrega el Proyecto del RelojTS

---
+++ app.ts
@@ -0,0 +1,10 @@
+const clock: HTMLSpanElement = document.getElementById('clock');
+
+setInterval(() => {
+    const date: Date = new Date();
+    clock.innerText = date.toLocaleTimeString('es-AR',{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+}, 1000);
